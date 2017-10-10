51a919db3f253adeb198acd3ebc533c922263861
Rafael Barros <rafael.rph7@gmail.com>
2017-10-10 12:58:10 -0300

    Decorator

--- alurabank/app/ts/models/Negociacoes.ts
+++ alurabank/app/ts/models/Negociacoes.ts
@@ -1,13 +1,16 @@
 import { Negociacao } from './Negociacao';
+import { LogarTempoExecucao } from '../helpers/decorators/LogarTempoExecucao';
 
 export class Negociacoes {
 
     private _negociacoes: Negociacao[] = [];
 
+    @LogarTempoExecucao()
     adiciona(negociacao: Negociacao): void {
         this._negociacoes.push(negociacao);
     }
 
+    @LogarTempoExecucao()
     paraArray(): Negociacao[] {
         return ([] as Negociacao[]).concat(this._negociacoes);
     }
